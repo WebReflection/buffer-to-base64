8c03f1a889f818075f24ed519838b557318f914d
webreflection <andrea.giammarchi@gmail.com>
2024-08-02 12:23:59 +0200

    Fix #4 - proper type in JSDoc TS

--- esm/encode.js
+++ esm/encode.js
@@ -2,7 +2,7 @@
  * Given a generic buffer, optionally compress it and returns its `base64` representation.
  * @param {ArrayBuffer | Uint8Array} buffer a generic buffer to optionally compress and return as base64.
  * @param {CompressionFormat | ''} format an optional compression format with `deflate` as default.
- * @returns {string} the base64 representation of the optionally compressed buffer.
+ * @returns {Promise<string>} the base64 representation of the optionally compressed buffer.
  */
 export const encode = async (buffer, format = 'deflate') => {
   for (var
